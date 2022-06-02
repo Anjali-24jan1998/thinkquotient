7f79ead50d3a2d2fddd5996447a979785f95d375
Anjali-24jan1998 <103317935+Anjali-24jan1998@users.noreply.github.com>
2022-06-03 01:07:14 +0530

    Add files via upload

---
+++ WebElement2.java
@@ -0,0 +1,36 @@
+package com.seleniumwebdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebElement2 {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		//Launch browser
+		WebDriver dr = new ChromeDriver();
+		
+		dr.get("https://online.actitime.com/thinkquotient/login.do");	//Opening actitime web site
+		dr.manage().window().maximize();
+		
+		Boolean display = dr.findElement(By.xpath("//div[@class='atProductName']")).isDisplayed();
+		System.out.println(display);
+		
+		WebElement wb = dr.findElement(By.id("keepLoggedInLabel"));
+		wb.click();
+		Thread.sleep(3000);
+		Boolean status1 = wb.isSelected();
+		System.out.println(status1);
+		
+		wb.click();
+		Boolean status2 = wb.isSelected();
+		System.out.println(status2);
+		
+		dr.close();
+
+
+	}
+
+}
--- webdriver3.java
+++ webdriver3.java
@@ -6,6 +6,7 @@ public class webdriver3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\SANKET\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	
 		//Launch browser
 		WebDriver dr = new ChromeDriver();
 		
